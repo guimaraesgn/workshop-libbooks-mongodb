53a7c5b382e6f42f32cd284844a60fd809a053b1
unknown <guimaraesgn30@gmail.com>
2023-08-01 22:07:58 -0300

    first commit, project created

---
+++ src/main/java/com/guimaraesgn/workshoplibbooksmongo/LibbooksmongoApplication.java
@@ -0,0 +1,13 @@
+package com.guimaraesgn.workshoplibbooksmongo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LibbooksmongoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LibbooksmongoApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/guimaraesgn/workshoplibbooksmongo/LibbooksmongoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.guimaraesgn.workshoplibbooksmongo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LibbooksmongoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
